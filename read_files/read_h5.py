3fe23c14b6989b6f5bbbf2c847746c8333012aa8
uoh-rislab <uoh.rislab@gmail.com>
2023-01-10 11:59:13 -0300

    read h5 files using python

---
+++ read_files/read_h5.py
@@ -0,0 +1,30 @@
+import numpy as np
+import h5py
+
+h5_file = '000000.h5'
+
+with h5py.File(h5_file, "r") as f:
+    # Print all root level object names (aka keys) 
+    # these can be group or dataset names 
+    print("Keys: %s" % f.keys())
+    
+    # get first object name/key; may or may NOT be a group
+    a_group_key = list(f.keys())[0]
+
+    # get the object type for a_group_key: usually group or dataset
+    print(type(f[a_group_key])) 
+
+    # If a_group_key is a group name, 
+    # this gets the object names in the group and returns as a list
+    data = list(f[a_group_key])
+
+    # If a_group_key is a dataset name, 
+    # this gets the dataset values and returns as a list
+    data = list(f[a_group_key])
+    
+    # preferred methods to get dataset values:
+    ds_obj = f[a_group_key]      # returns as a h5py dataset object
+    ds_arr = f[a_group_key][()]  # returns as a numpy array
+
+print('h5 file data: ', h5_file)
+
